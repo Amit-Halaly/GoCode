19a27ca759e0e1937600c832df25b77ec7a2a03f
Ben-Aharoni <aharoniben1111@gmail.com>
2025-12-23 21:46:08 +0200

    add lottie implementation and lottie icons for home screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,5 +56,6 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.play.services.auth)
+    implementation("com.airbnb.android:lottie:6.4.0")
 
 }
